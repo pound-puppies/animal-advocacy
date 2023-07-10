7b8f056c1934f9be7090580769c894f98c45fb4f
Esayas Asefa <esayas.t.asefa@gmail.com>
2023-07-10 13:06:51 -0500

    personal files created

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# animal-advocacy
+# Animal-Advocacy-Project
+### Brian Oneill, Scott Barnett, Keila Camarillo, and Esayas Asefa
+### 10 July 2023
+## Project description with goals
+### Description
+* We are tasked with answering the questions provided by our boss. Providing a professional email, final notebook, readme file, and google slide to the boss to address a minimum of 5 questions.
+
+### Goals¶
+* Construct an email answering at least 4 questions 
+* Deliver a final report to the data science team 
+* Deliver a slide with key points
+
+## Initial hypotheses and/or questions you have of the data, ideas
+
+* 1. ?
+* 2. ?
+* 3. ?
+* 4. ?
+
+*****************************************
+## Project Plan 
+* Data acquired from 
+    * Files were 
+* It contained ** rows and ** columns**
+* The data was aquired on **10 JULY 2023**
+* Each row represents an animals detection
+* Each column represents a feature of the access event
+* Prepare data
+
+   
+## Explore data in search of answers
+* Answer the following initial questions
+    * 1. 
+    * 2. 
+    * 3. 
+    * 4. 
+
+
+### Draw conclusions
+* 1.
+* 2. 
+* 3. 
+* 4. 
+
+## Data Dictionary
+
+## Steps to Reproduce
+* 1. 
+* 2. Clone this repo.
+* 3. 
+* 4. Run notebook.
+
+## Takeaways and Conclusions
+1. 
+
+2. 
+
+3. 
+
+4. 
+
+
+
+# Recommendations
+* Provide additional takeaways or downloadable docs for extensively used topics
+
+# Next Steps
+* 
---
+++ bo_functions.py
@@ -0,0 +1,72 @@
+######IMPORTS#####
+
+# Ignore Warnings
+import warnings
+warnings.filterwarnings("ignore")
+import wrangle as w
+
+import pandas as pd
+import numpy as np
+
+#splits
+from sklearn.model_selection import train_test_split
+
+#visualization
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import seaborn as sns
+
+#scaling
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler
+from sklearn.metrics import precision_score, accuracy_score, recall_score, classification_report
+
+#model
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.linear_model import LogisticRegression
+
+
+
+
+                                                            ##################### Functions for Exploration #########
+
+    
+    
+
+                                                            ################# Functions for Visualization ############
+
+        
+        
+
+
+                                                            ####################### Stats Functions ###################
+
+
+def eval_dist(r, p, α=0.05):
+    if p > α:
+        return print(f"""The data is normally distributed""")
+    else:
+        return print(f"""The data is NOT normally distributed""")   
+    
+def eval_Spearman(r, p, α=0.05):
+    if p < α:
+        return print(f"""We reject H₀, there is a monotonic relationship.
+Spearman's r: {r:2f}
+P-value: {p}""")
+    else:
+        return print(f"""We fail to reject H₀: that there is a monotonic relationship.
+Spearman's r: {r:2f}
+P-value: {p}""")
+
+    
+def eval_Pearson(r, p, α=0.05):
+    if p < α:
+        return print(f"""We reject H₀, there is a linear relationship with a Correlation Coefficient of {r:2f}.
+P-value: {p}""")
+    else:
+        return print(f"""We fail to reject H₀: that there is a linear relationship.
+Pearson's r: {r:2f}
+P-value: {p}""")
+
+
+                                                            ###################### Modeling Functions ##################
