9a7b177bba4273ecfe098fbe53df7b8168fe4072
Esayas Asefa <esayas.t.asefa@gmail.com>
2023-07-11 07:00:02 -0500

    first round of prepping completed

--- ea_wrangle.py
+++ ea_wrangle.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-import env
+# import env
 import os
 
 from sqlalchemy import text, create_engine
@@ -29,8 +29,52 @@ def get_aa_data(fn, query, url):
         df = pd.read_sql(query, url)
         df.to_csv(fn)
         return df
+    
+def left_join_csv(outcomes_file, intakes_file, merged_file):
+    # Read the CSV files
+    outcomes = pd.read_csv(outcomes_file)
+    intakes = pd.read_csv(intakes_file)
+
+    # Perform the left join
+    merged_data = pd.merge(outcomes, intakes, on='Animal ID', how='left')
+
+    # Save the merged data to a new CSV file
+    merged_data.to_csv(merged_file, index=False)    
+    
+    return merged_data
 
                                                         #################### Prepare Functions ##########################
 
-    
-    
+def get_prep_aa(df):
+    df.columns = df.columns.str.lower()
+
+    new_columns = {
+        'datetime_x': 'outcome_datetime',
+        'datetime_y': 'intake_datetime',
+        'monthyear_x': 'outcome_monthyear',
+        'monthyear_y': 'intake_monthyear',
+        'name_x': 'name',
+        'breed_y': 'breed',
+        'animal type_y': 'species',
+        'outcome type': 'outcome',
+        'color_y': 'color',
+        'sex upon outcome': 'outcome_sex',
+        'sex upon intake': 'intake_sex',
+        'intake type': 'intake_type',
+        'age upon intake': 'intake_age',
+        'age upon outcome': 'outcome_age',
+        'date of birth': 'dob',
+        'intake condition': 'intake_condition',
+        'found location': 'found_location'
+    }
+    df = df.rename(columns=new_columns)
+
+    columns_to_drop = ['outcome subtype', 'name_y', 'breed_x', 'animal type_x', 'color_x']
+    df = df.drop(columns=columns_to_drop)
+
+    df['outcome_datetime'] = pd.to_datetime(df['outcome_datetime'])
+    df['intake_datetime'] = pd.to_datetime(df['intake_datetime'])
+
+    df = df[df['species'].isin(['Cat', 'Dog'])]
+
+    return df
--- wrangle.py
+++ wrangle.py
@@ -32,5 +32,36 @@ def get_aa_data(fn, query, url):
 
                                                         #################### Prepare Functions ##########################
 
-    
-    
+def get_prep_aa(df):
+    df.columns = df.columns.str.lower()
+
+    new_columns = {
+        'datetime_x': 'outcome_datetime',
+        'datetime_y': 'intake_datetime',
+        'monthyear_x': 'outcome_monthyear',
+        'monthyear_y': 'intake_monthyear',
+        'name_x': 'name',
+        'breed_y': 'breed',
+        'animal type_y': 'species',
+        'outcome type': 'outcome',
+        'color_y': 'color',
+        'sex upon outcome': 'outcome_sex',
+        'sex upon intake': 'intake_sex',
+        'intake type': 'intake_type',
+        'age upon intake': 'intake_age',
+        'age upon outcome': 'outcome_age',
+        'date of birth': 'dob',
+        'intake condition': 'intake_condition',
+        'found location': 'found_location'
+    }
+    df = df.rename(columns=new_columns)
+
+    columns_to_drop = ['outcome subtype', 'name_y', 'breed_x', 'animal type_x', 'color_x']
+    df = df.drop(columns=columns_to_drop)
+
+    df['outcome_datetime'] = pd.to_datetime(df['outcome_datetime'])
+    df['intake_datetime'] = pd.to_datetime(df['intake_datetime'])
+
+    df = df[df['species'].isin(['Cat', 'Dog'])]
+
+    return df
