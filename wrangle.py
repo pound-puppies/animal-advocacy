dfbfb737f7eed0e9e00f3a84efd7eb0353ff1073
Esayas Asefa <esayas.t.asefa@gmail.com>
2023-07-11 10:14:31 -0500

    _

--- wrangle.py
+++ wrangle.py
@@ -15,19 +15,19 @@ np.random.seed(42)
 
                                                         ############### Acquire Functions ###########################
 
-def get_aa_data(fn, query, url):
-    """
-    check if file exists in my local directory, if not, pull from sql db
-    return dataframe
-    """
-    if os.path.isfile(fn):
-        print('csv file found and loaded')
-        return pd.read_csv(fn, index_col=0)
-    else:
-        print('creating df and exporting csv')
-        df = pd.read_sql(query, url)
-        df.to_csv(fn)
-        return df
+    
+def left_join_csv(outcomes_file, intakes_file, merged_file):
+    # Read the CSV files
+    outcomes = pd.read_csv(outcomes_file)
+    intakes = pd.read_csv(intakes_file)
+
+    # Perform the left join
+    merged_data = pd.merge(outcomes, intakes, on='Animal ID', how='left')
+
+    # Save the merged data to a new CSV file
+    merged_data.to_csv(merged_file, index=False)    
+    
+    return merged_data
 
                                                         #################### Prepare Functions ##########################
 
