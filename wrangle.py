bf50559993e827f3841bbe3942fa82373469f705
Esayas Asefa <esayas.t.asefa@gmail.com>
2023-07-12 11:04:25 -0500

    added and fixed age

--- wrangle.py
+++ wrangle.py
@@ -178,19 +178,52 @@ def get_prep_aa(df):
 
     # dropping unknown sex from df
     df = df[(df.intake_sex != 'unknown') & (df.intake_sex != 'nan')]
-
+    
+    # update data type
+    df.outcome_age = df.outcome_age.astype('int')
+    
+    # Define the conditions for each age category
+    conditions = [
+        (df['outcome_age'] <= 730),
+        (df['outcome_age'] >= 731) & (df['outcome_age'] <= 2920),
+        (df['outcome_age'] >= 2921)
+    ]
+    # Define the corresponding values for each age category
+    values = ['puppy', 'adult', 'senior']
+    # Create the 'age_category' column based on the conditions and values
+    df['age_category'] = pd.np.select(conditions, values, default='unknown')
     # keep these columns
-    keep_col= ['has_name', 'outcome', 'dob',
+    keep_col= ['has_name', 'outcome', 'dob', 'outcome_age',
                'species', 'intake_type', 'intake_condition',
                'intake_date', 'outcome_date', 'intake_age',
                'outcome_age', 'tenure_days', 'intake_sex',
                'breed', 'mix_breeds', 'two_breeds', 'pure_breed',
                'primary_color', 'is_tabby', 'mix_color']
     df = df[keep_col]
-
+    
     dummies_df = pd.get_dummies(df, columns=['outcome', 'species', 'intake_type',
-                                             'intake_condition', 'intake_sex', 'primary_color'], drop_first = True)
+                                             'intake_condition', 'intake_sex', 'primary_color', 'outcome_age'], drop_first = True)
     model_df = dummies_df.drop(columns=['dob', 'intake_date', 'outcome_date', 'breed'])
     return df, model_df
 
     return df
+
+#This confirms and Validates my split.
+def split_data(df, target_variable):
+    '''
+    Takes in two arguments the dataframe name and the ("target_variable" - must be in string format) to stratify  and
+    return train, validate, test subset dataframes will output train, validate, and test in that order.
+    '''
+    train, test = train_test_split(df, #first split
+                                   test_size=.2,
+                                   random_state=123,
+                                   stratify= df[target_variable])
+    train, validate = train_test_split(train, #second split
+                                       test_size=.25,
+                                       random_state=123,
+                                       stratify=train[target_variable])
+    print(f'train -> {train.shape}, {round(train.shape[0]*100 / df.shape[0],2)}%')
+    print(f'validate -> {validate.shape},{round(validate.shape[0]*100 / df.shape[0],2)}%')
+    print(f'test -> {test.shape}, {round(test.shape[0]*100 / df.shape[0],2)}%')
+    
+    return train, validate, test
